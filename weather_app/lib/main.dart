3d33e172548412be896ac03dc181c33e204db341
meissen <meissen54321@gmail.com>
2024-09-24 19:12:14 -0400

    Final modification

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -32,8 +32,9 @@ class _MyHomePageState extends State<MyHomePage> {
   //bool enable = true;
   String cityName = 'City Name';
   String cityNameUpdate = "";
-  int temp = 87;
-  String weather = 'rain';
+  int temp = 00;
+  String weather = 'IDK';
+  int zipcode=00000;
 
   @override
   Widget build(BuildContext context) {
@@ -83,6 +84,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 cityNameUpdate = cityName;
                 temp = 99;
                 weather = 'Sunny';
+                zipcode = 30096;
                 setState(() {                 
                 });
                 
@@ -90,6 +92,8 @@ class _MyHomePageState extends State<MyHomePage> {
               child: const Text('Get The Weather'),
             ),
             SizedBox(height: 16.0),
+            const SizedBox(height: 16.0),
+            const Text("Monday"),
 
 
             Row(
@@ -97,32 +101,220 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 Text(
                   'City Name: $cityNameUpdate  ',
-                  style: const TextStyle(fontSize: 20.0),
+                  style: const TextStyle(fontSize: 15.0),
                   )       ,
                 const SizedBox(height: 16.0),
 
                 Text(
                   'Temprature: $temp F  ',
-                  style: const TextStyle(fontSize: 20.0),
+                  style: const TextStyle(fontSize: 15.0),
                   )       ,
                 const SizedBox(height: 16.0),
 
                 Text(
                   'Weather Condition: $weather  ',
-                  style: const TextStyle(fontSize: 20.0),
+                  style: const TextStyle(fontSize: 15.0),
                   )       ,
                 const SizedBox(height: 16.0),
 
                 Text(
-                  ' Etc.',
-                  style: TextStyle(fontSize: 20.0),
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
                   )       ,
                 SizedBox(height: 16.0),
                 
               ],
-            )
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Tuesday"),
 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
 
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Wednesday"),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Thursday"),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Friday"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Saturday"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+            const SizedBox(height: 16.0),
+            const Text("Sunday"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name: $cityNameUpdate  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Temprature: $temp F  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'Weather Condition: $weather  ',
+                  style: const TextStyle(fontSize: 15.0),
+                  )       ,
+                const SizedBox(height: 16.0),
+
+                Text(
+                  'ZipCode: $zipcode',
+                  style: TextStyle(fontSize: 15.0),
+                  )       ,
+                SizedBox(height: 16.0),
+                
+              ],
+            ),
+           
           ]
 
         ),
